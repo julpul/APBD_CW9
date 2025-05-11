8a7a865f66014688735abedfc72105d8a5b4f302
Julian Pulawski <s31873@pjwstk.edu.pl>
2025-05-11 10:20:06 +0200

    project push na github

---
+++ Tutorial9 (1)/Tutorial9/Controllers/WarehouseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tutorial9.Model;
+
+
+namespace Tutorial9.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WarehouseController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public WarehouseController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+
+    public IActionResult Get([FromBody] ProductDTO product)
+    {
+        
+        
+        return Ok(product);
+    }
+    
+}
---
+++ Tutorial9 (1)/Tutorial9/Services/IProduct_WarehouseService.cs
@@ -0,0 +1,10 @@
+﻿using Tutorial9.Model;
+
+namespace Tutorial9.Services;
+
+public interface IProduct_WarehouseService
+{
+    Task<int> addProduct_Warehouse(ProductDTO product);
+    
+    
+}
